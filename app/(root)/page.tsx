112509d5bed2818219ff6073b1d300fdc56d7d69
Aurora <yingyun_zhang@yahoo.com>
2025-05-19 00:08:23 +0700

    wip: homepage, interviewCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
-
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Image from 'next/image';
+import { dummyInterviews } from '@/constants';
+import InterviewCard from '@/components/InterviewCard';
 const page = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-w-lg'>
+          <h2>Get Interview-Ready with AI-Powered Drills & Feedback </h2>
+          <p className='text-lg'>
+            Practice on real interview questions, get instant feedback, and
+            track progress.
+          </p>
+
+          <Button asChild className='btn-primary max-sm:w-full'>
+            <Link href='/interview'>Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src='/robot.png'
+          alt='robo-dude'
+          width={400}
+          height={400}
+          className='max-sm:hidden'
+          //No space to show this image on mobile.
+        />
+      </section>
+
+      <section className='flex felx-col gap-6 mt-8'>
+        <h2>Your Interviews</h2>
+
+        <div className='interviews-section'>
+          {/* <p>
+            No interviews yet. Start an interview and see track records here.
+          </p> */}
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Take an Interview</h2>
+
+        <div className='interviews-section'>
+          {/* <p>There are no interviews available</p> */}
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
---
+++ components/InterviewCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import dayjs from 'dayjs';
+import { cn, getRandomInterviewCover } from '@/lib/utils';
+import Image from 'next/image';
+const InterviewCard = ({
+  interviewId,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) => {
+  const feedback = null as Feedback | null;
+  // technical
+  // mix of techincal and behaviorial
+  const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
+  ///mix/gi 是一个正则表达式（Regular Expression）：
+  // /mix/ 是匹配模式，查找字符串中的 "mix"
+  // g 是全局标志（global flag），表示查找所 有匹配项
+  // i 是忽略大小写标志（case-insensitive flag），表示不区分大小写
+  const formattedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now()
+  ).format('MMM D, YYYY');
+
+  return (
+    <div className='card-border w-[360px] max-sm:w-full min-h-96'>
+      <div className='card-interview'>
+        <div>
+          <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
+            <p className='badge-text'>{normalizedType}</p>
+          </div>
+
+          <Image src={getRandomInterviewCover()}
+            alt="cover-image"
+            width={90}
+            height={90}
+            className="rounded-full object-fit size-[90px]"}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InterviewCard;
