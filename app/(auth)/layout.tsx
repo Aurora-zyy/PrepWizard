4d16dd46da06aa5492c93fbba1f5829706ef0e19
Aurora <yingyun_zhang@yahoo.com>
2025-05-24 12:19:59 +0700

    added redirecting:if auth->homepage;if !auth->sign-in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
 import React, { ReactNode } from 'react';
-
-const AuthLayout = ({ children }: { children: ReactNode }) => {
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isAuth = await isAuthenticated();
+  if (isAuth) {
+    redirect('/');
+  }
   return (
     <div className='auth-layout'>{children}</div>
     //here the class "auth-layout" is basiclaly vertically and horizontally centered
